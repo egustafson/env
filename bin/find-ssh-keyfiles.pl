06ecb348e050c99b36af777c4fc5b9ef40555376
Eric Gustafson <eg-github@elfwerks.org>
2013-01-21 11:26:07 -0700

    find-ssh-keyfiles.pl integration; used with Gentoo keychain

--- bin/find-ssh-keyfiles.pl
+++ bin/find-ssh-keyfiles.pl
@@ -4,10 +4,23 @@ open(SSHCFG, "$ENV{'HOME'}/.ssh/config");
 
 while (<SSHCFG>) {
     if ( /IdentityFile\s+(\S+)/ ) {
-        $pemfiles{"$1"} = 1;
+        $filen = $1;
+        $filen =~ s/^~(.*)/$ENV{'HOME'}$1/;
+        if ( -e $filen ) {
+            $pemfiles{"$filen"} = 1 if sshperms($filen);
+        }
     }
 }
 
 print join("\n", keys(%pemfiles));
 print "\n";
 
+##### Subroutines #####
+
+sub sshperms {
+    local($filename) = @_;
+    ($d,$i,$mode,$nlink,$uid) = stat($filename);
+    $readable = ( 0077 & $mode );
+    print STDERR "WARN: $filename permissions too open, skipping.\n" if $readable;
+    return !$readable
+}
