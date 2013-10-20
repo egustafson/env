04e41604455e3d023b420dceacc9837190916de0
Eric Gustafson <eg-git@elfwerks.org>
2013-10-19 18:04:04 -0600

    rc/emacs.d/init.el -- added '-diff' command line and customize diff screen

--- rc/emacs.d/init.el
+++ rc/emacs.d/init.el
@@ -28,6 +28,9 @@
 ;(setq-default compile-command "gmake")
 (setq-default compilation-read-command nil)
 
+(setq ediff-split-window-function 'split-window-horizontally)
+(setq ediff-merge-split-window-function 'split-window-horizontally)
+
 (add-to-list 'load-path (expand-file-name "~/.emacs.d"))
 
 (cond (window-system
@@ -258,6 +261,19 @@
 ;;;
 
 ;;(load "ada-mode-load")
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Command line '-diff' switch for ediff
+;;;
+;;;   reference:  http://www.emacswiki.org/emacs/EdiffMode
+;;
+(defun command-line-diff (switch)
+  (let ((file1 (pop command-line-args-left))
+        (file2 (pop command-line-args-left)))
+    (ediff file1 file2)))
+
+(add-to-list 'command-switch-alist '("diff" . command-line-diff))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;
